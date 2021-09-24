fb07bb28bc2e803034fae8e553d394bf8cb9dd18
José Cruz <jvsc@cin.ufpe.br>
2021-09-24 16:12:19 -0300

    refactor(token_manager): renames classes and file

--- api/token_manager.py
+++ api/token_manager.py
@@ -13,7 +13,7 @@ OK_STATUS_CODE: Final[int] = 200
 TOKEN_REFRESH_BEFORE_SECONDS: Final[int] = 10
 
 
-class Value:
+class TokenValues:
     access_token: str
     token_type: str
 
@@ -21,15 +21,15 @@ class Value:
         self.access_token, self.token_type = access_token, token_type
 
 
-class AwareNetworkingToken:
+class TokenManager:
     __client_id: str
     __client_secret: str
 
-    __value: Value
+    __token_values: TokenValues
     __expiration_time: dt.datetime
 
     @staticmethod
-    def __get_new_token(client_id: str, client_secret: str) -> Tuple[Value, dt.datetime]:
+    def __get_new_token(client_id: str, client_secret: str) -> Tuple[TokenValues, dt.datetime]:
         client_id_secret = client_id + ':' + client_secret
         base64url = base64.urlsafe_b64encode(client_id_secret.encode('ascii')).decode('utf-8')
         headers = {AUTHORIZATION_HEADER: 'Basic ' + base64url}
@@ -38,7 +38,7 @@ class AwareNetworkingToken:
 
         if response.status_code == OK_STATUS_CODE:
             parsed_response = json.loads(response.content.decode('utf-8'))
-            value = Value(parsed_response['access_token'], parsed_response['token_type'])
+            value = TokenValues(parsed_response['access_token'], parsed_response['token_type'])
             expiration_time = dt.datetime.now() + dt.timedelta(seconds=int(parsed_response['expires_in']))
             return value, expiration_time
         else:
@@ -46,12 +46,12 @@ class AwareNetworkingToken:
 
     def __init__(self, client_id: str, client_secret: str):
         self.__client_id, self.__client_secret = client_id, client_secret
-        self.__value, self.__expiration_time = self.__get_new_token(client_id, client_secret)
+        self.__token_values, self.__expiration_time = self.__get_new_token(client_id, client_secret)
 
     def __is_expired(self) -> bool:
         return (self.__expiration_time - dt.datetime.now()).total_seconds() <= TOKEN_REFRESH_BEFORE_SECONDS
 
-    def get(self) -> Value:
+    def get(self) -> TokenValues:
         if self.__is_expired():
-            self.__value, self.__expiration_time = self.__get_new_token(self.__client_id, self.__client_secret)
-        return self.__value
+            self.__token_values, self.__expiration_time = self.__get_new_token(self.__client_id, self.__client_secret)
+        return self.__token_values
