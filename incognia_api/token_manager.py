c43b12600fc472958a6c7148a26595749e01eec2
José Cruz <jvsc@cin.ufpe.br>
2021-09-28 00:41:00 -0300

    refactor(token_manager): fixes __get_new_token exception, applies suggested changes

--- incognia_api/token_manager.py
+++ incognia_api/token_manager.py
@@ -1,15 +1,14 @@
 import base64
 import datetime as dt
 import json
-from typing import Tuple, Final
+from typing import Final, Tuple
 
 import requests
 
 from endpoints import Endpoints
-from exceptions import IncogniaException
+from exceptions import IncogniaHTTPError
 
 AUTHORIZATION_HEADER: Final[str] = 'Authorization'
-OK_STATUS_CODE: Final[int] = 200
 TOKEN_REFRESH_BEFORE_SECONDS: Final[int] = 10
 
 
@@ -21,23 +20,25 @@ class TokenValues:
 class TokenManager:
     @staticmethod
     def __get_new_token(client_id: str, client_secret: str) -> Tuple[TokenValues, dt.datetime]:
-        client_id_secret = client_id + ':' + client_secret
-        base64url = base64.urlsafe_b64encode(client_id_secret.encode('ascii')).decode('utf-8')
+        client_id_and_secret = client_id + ':' + client_secret
+        base64url = base64.urlsafe_b64encode(client_id_and_secret.encode('ascii')).decode('utf-8')
         headers = {AUTHORIZATION_HEADER: 'Basic ' + base64url}
 
-        response = requests.post(url=Endpoints.TOKEN, headers=headers, auth=(client_id, client_secret))
+        try:
+            response = requests.post(url=Endpoints.TOKEN, headers=headers, auth=(client_id, client_secret))
+            response.raise_for_status()
 
-        if response.status_code == OK_STATUS_CODE:
             parsed_response = json.loads(response.content.decode('utf-8'))
-            value = TokenValues(parsed_response['access_token'], parsed_response['token_type'])
+            token_values = TokenValues(parsed_response['access_token'], parsed_response['token_type'])
             expiration_time = dt.datetime.now() + dt.timedelta(seconds=int(parsed_response['expires_in']))
-            return value, expiration_time
-        else:
-            raise IncogniaException('network error: {}'.format(response.status_code))
+
+            return token_values, expiration_time
+        except requests.HTTPError as e:
+            raise IncogniaHTTPError(e)
 
     def __init__(self, client_id: str, client_secret: str):
         self.__client_id, self.__client_secret = client_id, client_secret
-        self.__token_values, self.__expiration_time = self.__get_new_token(client_id, client_secret)
+        self.__token_values, self.__expiration_time = self.__get_new_token(self.__client_id, self.__client_secret)
 
     def __is_expired(self) -> bool:
         return (self.__expiration_time - dt.datetime.now()).total_seconds() <= TOKEN_REFRESH_BEFORE_SECONDS
