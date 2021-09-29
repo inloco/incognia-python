f639f22a36600283c374ba5814c2a0f74f619311
José Cruz <jvsc@cin.ufpe.br>
2021-09-29 19:04:56 -0300

    refactor(endpoints): removes urljoin

--- incognia_api/endpoints.py
+++ incognia_api/endpoints.py
@@ -1,10 +1,9 @@
 from typing import Final
-from urllib.parse import urljoin
 
 
 class Endpoints:
     BASE: Final[str] = 'https://api.us.incognia.com'
 
-    TOKEN: Final[str] = urljoin(BASE, 'api/v1/token')
-    SIGNUPS: Final[str] = urljoin(BASE, 'api/v2/onboarding/signups')
-    TRANSACTIONS: Final[str] = urljoin(BASE, 'api/v2/authentication/transactions')
+    TOKEN: Final[str] = f'{BASE}/api/v1/token'
+    SIGNUPS: Final[str] = f'{BASE}/api/v2/onboarding/signups'
+    TRANSACTIONS: Final[str] = f'{BASE}/api/v2/authentication/transactions'
