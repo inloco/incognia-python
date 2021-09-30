a0c8f260844e1a86a0bc067d9d226d5d84ad33e3
José Cruz <jvsc@cin.ufpe.br>
2021-09-30 01:15:53 -0300

    feat(setup.py): creates the file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='incognia-python',
+    version='1.0.0',
+    packages=find_packages(exclude=['tests']),
+    url='',
+    license='',
+    author='incognia',
+    author_email='',
+    description='Incognia API Python Client',
+    python_requires='>=3.8'
+)
