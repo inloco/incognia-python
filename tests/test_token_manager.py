69f64c08c8b6d8528becfc2fd4e1587a66945738
José Cruz <jvsc@cin.ufpe.br>
2021-10-01 14:41:22 -0300

    test(test_token_manager): fixes tests mocked requirements

--- tests/test_token_manager.py
+++ tests/test_token_manager.py
@@ -1,8 +1,11 @@
-import datetime as dt
+import base64
 from typing import Final
 from unittest import TestCase
 from unittest.mock import Mock, patch
 
+import requests
+
+from incognia_api.endpoints import Endpoints
 from incognia_api.exceptions import IncogniaHTTPError
 from incognia_api.token_manager import TokenManager, TokenValues
 
@@ -11,55 +14,73 @@ class TestTokenManager(TestCase):
     CLIENT_ID: Final[str] = 'ANY_ID'
     CLIENT_SECRET: Final[str] = 'ANY_SECRET'
     TOKEN_VALUES: Final[TokenValues] = TokenValues('ACCESS_TOKEN', 'TOKEN_TYPE')
-    SECOND_TOKEN_VALUES: Final[TokenValues] = TokenValues('SECOND_ACCESS_TOKEN', 'SECOND_TOKEN_TYPE')
-    EXPIRATION_TIME: Final[dt.datetime] = dt.datetime.fromisoformat('2000-01-01')
+    JSON_POST_RESPONSE: Final[ascii] = b'{ "access_token": "ACCESS_TOKEN", "token_type": "TOKEN_TYPE",' \
+                                       b' "expires_in": 900 }'
+    SHORT_EXPIRATION_TIME_TOKEN_VALUES: Final[TokenValues] = TokenValues('S_E_T_ACCESS_TOKEN', 'S_E_T_TOKEN_TYPE')
+    SHORT_EXPIRATION_TIME_JSON_POST_RESPONSE: Final[ascii] = b'{ "access_token": "S_E_T_ACCESS_TOKEN",' \
+                                                             b' "token_type": "S_E_T_TOKEN_TYPE", "expires_in": 5 }'
+    BASE64URL: Final[str] = base64.urlsafe_b64encode(f'{CLIENT_ID}:{CLIENT_SECRET}'.encode('ascii')).decode('utf-8')
+    HEADERS: Final[dict] = {'Authorization': f'Basic {BASE64URL}'}
+    OK_STATUS_CODE: Final[int] = 200
+    CLIENT_ERROR_CODE: Final[int] = 400
 
-    @patch.object(TokenManager, 'refresh_token')
-    def test_get_when_credentials_are_valid_should_return_a_valid_token(self, mock_refresh_token: Mock):
+    @patch('requests.post')
+    def test_get_when_credentials_are_valid_should_return_a_valid_token(self, mock_requests_post: Mock):
+        def get_mocked_response() -> requests.Response:
+            response = requests.Response()
+            response._content, response.status_code = self.JSON_POST_RESPONSE, self.OK_STATUS_CODE
+            return response
+
+        mock_requests_post.configure_mock(return_value=get_mocked_response())
+
         token_manager = TokenManager(self.CLIENT_ID, self.CLIENT_SECRET)
-
-        def refresh_token_side_effect():
-            token_manager._TokenManager__token_values = self.TOKEN_VALUES
-
-        mock_refresh_token.configure_mock(side_effect=refresh_token_side_effect)
-
         token_values = token_manager.get()
-        mock_refresh_token.assert_called()
+        mock_requests_post.assert_called_with(url=Endpoints.TOKEN, headers=self.HEADERS,
+                                              auth=(self.CLIENT_ID, self.CLIENT_SECRET))
         self.assertEqual(token_values, self.TOKEN_VALUES)
 
-    @patch.object(TokenManager, 'refresh_token')
-    def test_get_when_credentials_are_invalid_should_raise_an_IncogniaHTTPError(self, mock_refresh_token: Mock):
-        mock_refresh_token.configure_mock(side_effect=IncogniaHTTPError())
+    @patch('requests.post')
+    def test_get_when_credentials_are_invalid_should_raise_an_IncogniaHTTPError(self, mock_requests_post: Mock):
+        def get_mocked_response() -> requests.Response:
+            response = requests.Response()
+            response.status_code = self.CLIENT_ERROR_CODE
+            return response
+
+        mock_requests_post.configure_mock(return_value=get_mocked_response())
+
         token_manager = TokenManager(self.CLIENT_ID, self.CLIENT_SECRET)
         self.assertRaises(IncogniaHTTPError, token_manager.get)
-        mock_refresh_token.assert_called()
+        mock_requests_post.assert_called_with(url=Endpoints.TOKEN, headers=self.HEADERS,
+                                              auth=(self.CLIENT_ID, self.CLIENT_SECRET))
 
-    @patch.object(TokenManager, 'is_expired')
-    @patch.object(TokenManager, 'refresh_token')
-    def test_get_when_token_is_expired_should_return_a_new_valid_token(self, mock_refresh_token: Mock,
-                                                                       mock_is_expired: Mock):
+    @patch('requests.post')
+    def test_get_when_token_is_expired_should_return_a_new_valid_token(self, mock_requests_post: Mock):
         token_manager = TokenManager(self.CLIENT_ID, self.CLIENT_SECRET)
 
-        def refresh_token_first_side_effect():
-            token_manager._TokenManager__token_values = self.TOKEN_VALUES
-            token_manager._TokenManager__expiration_time = self.EXPIRATION_TIME
+        def get_first_mocked_response() -> requests.Response:
+            response = requests.Response()
+            response._content, response.status_code = self.SHORT_EXPIRATION_TIME_JSON_POST_RESPONSE, self.OK_STATUS_CODE
+            return response
 
-        mock_refresh_token.configure_mock(side_effect=refresh_token_first_side_effect)
+        mock_requests_post.configure_mock(return_value=get_first_mocked_response())
 
         first_token_values = token_manager.get()
-        mock_refresh_token.assert_called()
 
-        def refresh_token_second_side_effect():
-            token_manager._TokenManager__token_values = self.SECOND_TOKEN_VALUES
-            token_manager._TokenManager__expiration_time = self.EXPIRATION_TIME + dt.timedelta(seconds=5)
+        mock_requests_post.assert_called_with(url=Endpoints.TOKEN, headers=self.HEADERS,
+                                              auth=(self.CLIENT_ID, self.CLIENT_SECRET))
 
-        mock_is_expired.configure_mock(return_value=True)
-        mock_refresh_token.reset_mock()
-        mock_refresh_token.configure_mock(side_effect=refresh_token_second_side_effect)
+        def get_second_mocked_response() -> requests.Response:
+            response = requests.Response()
+            response._content, response.status_code = self.JSON_POST_RESPONSE, self.OK_STATUS_CODE
+            return response
+
+        mock_requests_post.reset_mock()
+        mock_requests_post.configure_mock(return_value=get_second_mocked_response())
 
         second_token_values = token_manager.get()
-        mock_refresh_token.assert_called()
-        mock_is_expired.assert_called()
 
-        self.assertEqual(first_token_values, self.TOKEN_VALUES)
-        self.assertEqual(second_token_values, self.SECOND_TOKEN_VALUES)
+        mock_requests_post.assert_called_with(url=Endpoints.TOKEN, headers=self.HEADERS,
+                                              auth=(self.CLIENT_ID, self.CLIENT_SECRET))
+
+        self.assertEqual(first_token_values, self.SHORT_EXPIRATION_TIME_TOKEN_VALUES)
+        self.assertEqual(second_token_values, self.TOKEN_VALUES)
