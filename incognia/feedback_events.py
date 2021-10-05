8cf5b8d4f091df9a7f1f240fbc42d1fb3ee700e4
José Cruz <jvsc@cin.ufpe.br>
2021-10-05 02:47:30 -0300

    refactor(FeedbackEvents): adds, PAYMENT_ACCEPTED_BY_THIRD_PARTY, fixes PAYMENT_DECLINED_BY_RISK_ANALYSIS

--- incognia/feedback_events.py
+++ incognia/feedback_events.py
@@ -4,20 +4,21 @@ FeedbackEventType = str
 
 
 class FeedbackEvents:
-    SIGNUP_ACCEPTED: Final[FeedbackEventType] = "signup_accepted"
-    SIGNUP_DECLINED: Final[FeedbackEventType] = "signup_declined"
-    PAYMENT_ACCEPTED: Final[FeedbackEventType] = "payment_accepted"
-    PAYMENT_DECLINED: Final[FeedbackEventType] = "payment_declined"
-    PAYMENT_DECLINED___BY_RISK_ANALYSIS: Final[FeedbackEventType] = \
-        "payment_declined___by_risk_analysis"
+    SIGNUP_ACCEPTED: Final[FeedbackEventType] = 'signup_accepted'
+    SIGNUP_DECLINED: Final[FeedbackEventType] = 'signup_declined'
+    PAYMENT_ACCEPTED: Final[FeedbackEventType] = 'payment_accepted'
+    PAYMENT_ACCEPTED_BY_THIRD_PARTY: Final[FeedbackEventType] = 'payment_accepted_by_third_party'
+    PAYMENT_DECLINED: Final[FeedbackEventType] = 'payment_declined'
+    PAYMENT_DECLINED_BY_RISK_ANALYSIS: Final[FeedbackEventType] = \
+        'payment_declined_by_risk_analysis'
     PAYMENT_DECLINED_BY_MANUAL_REVIEW: Final[FeedbackEventType] = \
-        "payment_declined_by_manual_review"
-    PAYMENT_DECLINED_BY_BUSINESS: Final[FeedbackEventType] = "payment_declined_by_business"
-    PAYMENT_DECLINED_BY_ACQUIRER: Final[FeedbackEventType] = "payment_declined_by_acquirer"
-    LOGIN_ACCEPTED: Final[FeedbackEventType] = "login_accepted"
-    LOGIN_DECLINED: Final[FeedbackEventType] = "login_declined"
-    VERIFIED: Final[FeedbackEventType] = "verified"
-    NOT_VERIFIED: Final[FeedbackEventType] = "not_verified"
-    ACCOUNT_TAKEOVER: Final[FeedbackEventType] = "account_takeover"
-    CHARGEBACK: Final[FeedbackEventType] = "chargeback"
-    MPOS_FRAUD: Final[FeedbackEventType] = "mpos_fraud"
+        'payment_declined_by_manual_review'
+    PAYMENT_DECLINED_BY_BUSINESS: Final[FeedbackEventType] = 'payment_declined_by_business'
+    PAYMENT_DECLINED_BY_ACQUIRER: Final[FeedbackEventType] = 'payment_declined_by_acquirer'
+    LOGIN_ACCEPTED: Final[FeedbackEventType] = 'login_accepted'
+    LOGIN_DECLINED: Final[FeedbackEventType] = 'login_declined'
+    VERIFIED: Final[FeedbackEventType] = 'verified'
+    NOT_VERIFIED: Final[FeedbackEventType] = 'not_verified'
+    ACCOUNT_TAKEOVER: Final[FeedbackEventType] = 'account_takeover'
+    CHARGEBACK: Final[FeedbackEventType] = 'chargeback'
+    MPOS_FRAUD: Final[FeedbackEventType] = 'mpos_fraud'
