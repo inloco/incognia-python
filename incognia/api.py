e7150eda244c36088ab206a6e4fa39d169bcfdc0
José Cruz <jvsc@cin.ufpe.br>
2021-10-06 16:42:20 -0300

    feat(api): adds send_feedback method

--- incognia/api.py
+++ incognia/api.py
@@ -1,3 +1,4 @@
+import datetime as dt
 import json
 from typing import Optional, Literal
 
@@ -5,6 +6,7 @@ import requests
 
 from .endpoints import Endpoints
 from .exceptions import IncogniaHTTPError, IncogniaError
+from .feedback_events import FeedbackEventType
 from .models import Coordinates, StructuredAddress
 from .token_manager import TokenManager
 
@@ -60,3 +62,45 @@ class IncogniaAPI:
 
         except requests.HTTPError as e:
             raise IncogniaHTTPError(e) from None
+
+    def send_feedback(self,
+                      event: FeedbackEventType,
+                      timestamp: dt.datetime,
+                      external_id: Optional[str] = None,
+                      login_id: Optional[str] = None,
+                      payment_id: Optional[str] = None,
+                      signup_id: Optional[str] = None,
+                      account_id: Optional[str] = None,
+                      installation_id: Optional[str] = None):
+        if not event:
+            raise IncogniaError('event is required.')
+        if not timestamp:
+            raise IncogniaError('timestamp is required.')
+
+        try:
+            access_token, token_type = self.__token_manager.get()
+            headers = {
+                'Content-type': 'application/json',
+                'Authorization': f'{token_type} {access_token}'
+            }
+
+            def total_milliseconds(t: dt.datetime):
+                return (t - dt.datetime.utcfromtimestamp(0)).total_seconds() * 1000.0
+
+            body = {
+                'event': event,
+                'timestamp': total_milliseconds(timestamp),
+                'external_id': external_id,
+                'login_id': login_id,
+                'payment_id': payment_id,
+                'signup_id': signup_id,
+                'account_id': account_id,
+                'installation_id': installation_id
+            }
+            data = json.dumps({k: v for (k, v) in body.items() if v is not None})
+            response = requests.post(self.__endpoints.feedbacks, headers=headers, data=data)
+            response.raise_for_status()
+
+            return json.loads(response.content.decode('utf-8'))
+        except requests.HTTPError as e:
+            raise IncogniaHTTPError(e)
