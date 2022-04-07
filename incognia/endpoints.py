0b96fb33c53945ac1b4ee7611aa84fedc765db0f
José Cruz <jose.cruz@incognia.com>
2022-04-07 18:29:50 -0300

    refactor(endpoints): changes token route to v2

--- incognia/endpoints.py
+++ incognia/endpoints.py
@@ -4,7 +4,7 @@ from typing import Final
 class Endpoints:
     BASE: Final[str] = 'https://api.incognia.com'
 
-    TOKEN: Final[str] = f'{BASE}/api/v1/token'
+    TOKEN: Final[str] = f'{BASE}/api/v2/token'
     SIGNUPS: Final[str] = f'{BASE}/api/v2/onboarding/signups'
     FEEDBACKS: Final[str] = f'{BASE}/api/v2/feedbacks'
     TRANSACTIONS: Final[str] = f'{BASE}/api/v2/authentication/transactions'
